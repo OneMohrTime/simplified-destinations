8b7cce9410443420433893639cc3ac578ab3dac7
Derek Mohr <derek@onemohrti.me>
2024-08-24 19:32:09 -0400

    Create scroll module for modujs

--- wp-content/themes/artprize/src/scripts/modules.js
+++ wp-content/themes/artprize/src/scripts/modules.js
@@ -11,5 +11,5 @@ export { default as Fancybox } from './modules/Fancybox';
 export { default as Fitty } from './modules/Fitty';
 // export { default as Load } from './modules/Load';
 export { default as Navigation } from './modules/Navigation';
-// export { default as Scroll } from './modules/Scroll';
+export { default as Scroll } from './modules/Scroll';
 export { default as Swiper } from './modules/Swiper';
